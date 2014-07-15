1e0c123d5b96021b82157b5e7bd27d60a0fdd5df
Jordan OMara <jomara@redhat.com>
2014-07-15 12:16:11 -0400

    moving code to overrides

--- tuskar_sat_ui/__init__.py
+++
@@ -1,2 +0,0 @@
-from tuskar_sat_ui.infrastructure import nodes
-from tuskar_sat_ui.api import node
---
+++ tuskar_sat_ui/overrides/infrastructure/node_views.py
@@ -0,0 +1,3 @@
+from tuskar_ui.infrastructure.nodes import views as nodes_views
+
+nodes_views.DetailView.template_name = 'tuskar_sat_ui/overrides/infrastructure/templates/nodes/details.html'
