05c3a99b3f32611c6ea02ed279298359b9037090
Radomir Dopieralski <openstack@sheep.art.pl>
2014-10-31 14:33:26 +0100

    Make tuskar_sat_ui installable by copying a panel file
    Change-Id: Ie60eb74557609439b0c524d866fe6974a84267b1

---
+++ _60_tuskar_sat_ui.py.example
@@ -0,0 +1,6 @@
+PANEL = 'nodes'
+PANEL_DASHBOARD = 'infrastructure'
+ADD_PANEL = 'tuskar_sat_ui.nodes.panel.Nodes'
+ADD_INSTALLED_APPS = [
+    'tuskar_sat_ui',
+]
---
+++ tuskar_sat_ui/nodes/urls.py
@@ -0,0 +1,34 @@
+# -*- coding: utf8 -*-
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from django.conf import urls
+
+from tuskar_ui.infrastructure.nodes import views
+from tuskar_sat_ui.nodes import views as sat_views
+
+
+urlpatterns = urls.patterns(
+    '',
+    urls.url(r'^$', views.IndexView.as_view(), name='index'),
+    urls.url(r'^register/$', views.RegisterView.as_view(),
+             name='register'),
+    urls.url(r'^auto-discover-csv/$', views.UploadView.as_view(),
+             name='auto-discover-csv'),
+    urls.url(r'^nodes_performance/$',
+             views.PerformanceView.as_view(), name='nodes_performance'),
+    urls.url(r'^(?P<node_uuid>[^/]+)/$', sat_views.DetailView.as_view(),
+             name='detail'),
+    urls.url(r'^(?P<node_uuid>[^/]+)/performance/$',
+             views.PerformanceView.as_view(), name='performance'),
+)
