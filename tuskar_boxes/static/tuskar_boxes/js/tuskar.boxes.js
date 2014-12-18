a2a5fe4db60846908019a965f40bf57ebe98547c
Radomir Dopieralski <openstack@sheep.art.pl>
2014-12-18 09:21:13 +0100

    Fix alignment of the edit role icon in the plan edit view
    * Aligment done with grid, not pull-right
    * Correct color for the icon
    * Showing/hiding done with CSS not JS
    * Repeated part of the template moved to an include
    
    Change-Id: I3e1c81b885f9b0df11d7826d8c26dcf24d622e01

--- tuskar_boxes/static/tuskar_boxes/js/tuskar.boxes.js
+++ tuskar_boxes/static/tuskar_boxes/js/tuskar.boxes.js
@@ -82,7 +82,6 @@ tuskar.boxes = (function () {
           ui.draggable.find('input.boxes-flavor'
               ).val($(this).closest('.boxes-flavor').data('flavor'));
           $count.trigger('change');
-          ui.draggable.find('a[name^=role-edit]').removeClass('hidden');
           window.setTimeout(update_boxes, 0);
         }
     });
@@ -95,7 +94,6 @@ tuskar.boxes = (function () {
           ui.draggable.appendTo(this);
           ui.draggable.find('input.boxes-flavor').val('');
           ui.draggable.find('input.number-picker').trigger('change').val(0);
-          ui.draggable.find('a[name^=role-edit]').addClass('hidden');
           window.setTimeout(update_boxes, 0);
         }
     });
