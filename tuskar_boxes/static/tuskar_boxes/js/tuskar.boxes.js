4c4c948b05788a665011f05c57a3c230fc3a63e5
Radomir Dopieralski <openstack@sheep.art.pl>
2015-01-20 13:24:42 +0100

    Some basic unit tests for the tuskar.boxes js module
    Change-Id: I6b15ec7d818ad7d314954674641983a6c5ff8033

--- tuskar_boxes/static/tuskar_boxes/js/tuskar.boxes.js
+++ tuskar_boxes/static/tuskar_boxes/js/tuskar.boxes.js
@@ -3,67 +3,67 @@ tuskar.boxes = (function () {
 
   var module = {};
 
+
+  module.get_role_counts = function ($flavor) {
+    var roles = {};
+    $flavor.find('div.boxes-drop-roles div.boxes-role').each(function () {
+      var $this = $(this);
+      var name = $this.data('name');
+      var count = +$this.find('input.number-picker').val();
+      roles[name] = count;
+    });
+    return roles;
+  };
+
+  module.update_maximums = function ($flavor, roles, nodes) {
+    var used = 0;
+    $.each(roles, function (key, value) { used += value; });
+    $flavor.find('div.boxes-drop-roles div.boxes-role').each(function () {
+      var $this = $(this);
+      var role = $this.data('name');
+      var $picker = $this.find('input.number-picker');
+      $picker.attr('max', Math.max(0, nodes - used + roles[role]));
+    });
+  };
+
+  module.update_nodes = function ($flavor, roles) {
+    var role_names = Object.getOwnPropertyNames(roles);
+    var count = 0;
+    var role = 0;
+    $flavor.find('div.boxes-nodes div.boxes-node').each(function () {
+      var $this = $(this);
+      $this.removeClass('boxes-role-controller boxes-role-compute boxes-role-cinder-storage boxes-role-swift-storage boxes-role-none');
+      while (count >= roles[role_names[role]]) {
+        role += 1;
+        count = 0;
+      }
+      if (!role_names[role]) {
+        $(this).html('free');
+        $(this).addClass('boxes-role-none');
+      } else {
+        $this.addClass('boxes-role-' + role_names[role]).html('&nbsp;');
+      }
+      count += 1;
+    });
+    var free_nodes = $flavor.find('div.boxes-nodes div.boxes-role-none').length;
+    $flavor.find('span.free-nodes').text(free_nodes);
+  };
+
+  module.update_boxes = function () {
+    $('div.boxes-flavor').each(function () {
+        var $flavor = $(this);
+        var role_counts = module.get_role_counts($flavor);
+        var nodes_count = $flavor.find('div.boxes-nodes div.boxes-node').length;
+        module.update_nodes($flavor, role_counts);
+        module.update_maximums($flavor, role_counts, nodes_count);
+    });
+  };
+
   module.init = function () {
     if ($('div.boxes-available-roles').length === 0) {
       // Only activate on a page that has the right classes.
       return;
     }
-
-    function get_role_counts($flavor) {
-      var roles = {};
-      $flavor.find('div.boxes-drop-roles div.boxes-role').each(function () {
-        var $this = $(this);
-        var name = $this.data('name');
-        var count = +$this.find('input.number-picker').val();
-        roles[name] = count;
-      });
-      return roles;
-    }
-
-    function update_maximums($flavor, roles) {
-      var nodes = $flavor.find('div.boxes-nodes div.boxes-node').length;
-      var used = 0;
-      $.each(roles, function (key, value) { used += value; });
-      $flavor.find('div.boxes-drop-roles div.boxes-role').each(function () {
-        var $this = $(this);
-        var role = $this.data('name');
-        var $picker = $this.find('input.number-picker');
-        $picker.attr('max', Math.max(0, nodes - used + roles[role]));
-      });
-    }
-
-    function update_nodes($flavor, roles) {
-      var role_names = Object.getOwnPropertyNames(roles);
-      var count = 0;
-      var role = 0;
-      $flavor.find('div.boxes-nodes div.boxes-node').each(function () {
-        var $this = $(this);
-        $this.removeClass('boxes-role-controller boxes-role-compute boxes-role-cinder-storage boxes-role-swift-storage boxes-role-none');
-        while (count >= roles[role_names[role]]) {
-          role += 1;
-          count = 0;
-        }
-        if (!role_names[role]) {
-          $(this).html('free');
-          $(this).addClass('boxes-role-none');
-        } else {
-          $this.addClass('boxes-role-' + role_names[role]).html('&nbsp;');
-        }
-        count += 1;
-      });
-      var free_nodes = $flavor.find('div.boxes-nodes div.boxes-role-none').length;
-      $flavor.find('span.free-nodes').text(free_nodes);
-    }
-
-    function update_boxes() {
-      $('div.boxes-flavor').each(function () {
-          var $flavor = $(this);
-          var roles = get_role_counts($flavor);
-          update_nodes($flavor, roles);
-          update_maximums($flavor, roles);
-      });
-    }
-
     $('div.boxes-role').draggable({
         revert: 'invalid',
         helper: 'clone',
@@ -82,7 +82,7 @@ tuskar.boxes = (function () {
           ui.draggable.find('input.boxes-flavor'
               ).val($(this).closest('.boxes-flavor').data('flavor'));
           $count.trigger('change');
-          window.setTimeout(update_boxes, 0);
+          window.setTimeout(module.update_boxes, 0);
         }
     });
     $('div.boxes-available-roles').droppable({
@@ -94,12 +94,12 @@ tuskar.boxes = (function () {
           ui.draggable.appendTo(this);
           ui.draggable.find('input.boxes-flavor').val('');
           ui.draggable.find('input.number-picker').trigger('change').val(0);
-          window.setTimeout(update_boxes, 0);
+          window.setTimeout(module.update_boxes, 0);
         }
     });
 
-    update_boxes();
-    $('input.number-picker').change(update_boxes);
+    module.update_boxes();
+    $('input.number-picker').change(module.update_boxes);
 
     $('.boxes-roles-menu li a').click(function () {
         var name = $(this).data('role');
@@ -111,7 +111,7 @@ tuskar.boxes = (function () {
         if (+$count.val() < 1) { $count.val(1); }
         $flavor.val($drop.closest('.boxes-flavor').data('flavor'));
         $count.trigger('change');
-        window.setTimeout(update_boxes, 0);
+        window.setTimeout(module.update_boxes, 0);
     });
   };
 
